0ca647c38c842d430a87a5be7f4ff3deee55755e
MevawalaBhavya <bhavyamevawala18@gmail.com>
2025-03-04 00:02:45 -0400

    new programs

--- Pascals-triangle.java
+++ Pascals-triangle.java
@@ -1,22 +1,24 @@
+import java.util.List;
+
 class Solution {
 
-    Public static void main(String args[]){
-        getRow(2);
-    }
-
-    public static List<Integer> getRow(int rowIndex) {
-
-        List<Integer> triange = new List<>();
-        triange.add(0,1);
-        triange.add(rowIndex-1,1);
-
-        int sum = 0;
-        for(int i=1; i<rowIndex; i++){
-            
-            sum = triange.get(i-1) + triange.get(i+1);
-            triange.add(i,sum);
-        }
-        
-         return triange;
-    }
+//    public static void main(String args[]){
+//        getRow(2);
+//    }
+//
+//    public static List<Integer> getRow(int rowIndex) {
+//
+//        List<Integer> triangle = new List<Integer>();
+//        triangle.add(0,1);
+//        triangle.add(rowIndex-1,1);
+//
+//        int sum = 0;
+//        for(int i=1; i<rowIndex; i++){
+//
+//            sum = triangle.get(i-1) + triangle.get(i+1);
+//            triangle.add(i,sum);
+//        }
+//
+//         return triangle;
+//    }
 }
